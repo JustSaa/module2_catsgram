8af3f54cfe5cb30514cff7ba8e70830d5671a75f
Salavat <Justdrumm@yandex.ru>
2023-12-10 20:30:35 +0300

    feat:Added request paging to Post

--- src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
+++ src/main/java/ru/yandex/practicum/catsgram/controller/PostController.java
@@ -1,9 +1,11 @@
 package ru.yandex.practicum.catsgram.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.catsgram.model.Post;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,9 +24,21 @@ public class PostController {
     }
 
     @GetMapping("/posts")
-    public List<Post> findAll() {
-        log.debug("Текущее количество постов: {}", postService.findAll().size());
-        return postService.findAll();
+    public List<Post> findAll(
+            @RequestParam(value = "sort", defaultValue = "desc", required = false) String sort,
+            @RequestParam(value = "size", defaultValue = "10", required = false) Integer size,
+            @RequestParam(value = "page", defaultValue = "0", required = false) Integer page
+    ) {
+        if (!(sort.equals("desc") || sort.equals("acs"))) {
+            throw new IllegalArgumentException();
+        }
+        if (page < 0 || size <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        Integer from = page * size;
+        //log.debug("Текущее количество постов: {}", postService.findAll().size());
+        return postService.findAll(sort, size, from);
     }
 
     @PostMapping(value = "/post")
@@ -35,5 +49,33 @@ public class PostController {
     @GetMapping("/post/{id}")
     public Optional<Post> getById(@PathVariable int id) {
         return postService.getById(id);
+    }
+
+    @GetMapping("/posts/search")
+    public List<Post> searchPosts(@RequestParam String author) {
+        System.out.println("Ищем посты пользователя с именем " + author);
+        return postService.searchPosts(author);
+    }
+
+    @GetMapping("/posts/search")
+    public List<Post> searchPosts(
+            @RequestParam String author,
+            @RequestParam
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+
+        System.out.println("Ищем посты пользователя с именем " + author +
+                " и опубликованные " + date);
+        return postService.searchPosts(author, date);
+    }
+
+    @GetMapping("/users/{userId}/posts/list")
+    public List<Post> listPosts(
+            @PathVariable Integer userId,
+            @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate from,
+            @RequestParam @DateTimeFormat(pattern = "dd.MM.yyyy") LocalDate to
+    ) {
+        System.out.println("Ищем посты пользователя " + userId +
+                " с даты " + from.toString() + " по дату " + to.toString());
+        return postService.listPosts(userId, from, to);
     }
 }
--- src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/PostService.java
@@ -3,16 +3,26 @@ package ru.yandex.practicum.catsgram.service;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.catsgram.model.Post;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PostService {
     private final List<Post> posts = new ArrayList<>();
 
-    public List<Post> findAll() {
-        return posts;
+    public List<Post> findAll(String sort, Integer size, Integer from) {
+        return posts.stream().sorted((p0, p1) -> {
+            int comp = p0.getCreationDate().compareTo(p1.getCreationDate()); //прямой порядок сортировки
+            if(sort.equals("desc")){
+                comp = -1 * comp; //обратный порядок сортировки
+            }
+            return comp;
+        }).skip(from).limit(size).collect(Collectors.toList());
     }
 
     public Post create(Post post) {
@@ -22,5 +32,26 @@ public class PostService {
 
     public Optional<Post> getById(int id) {
         return posts.stream().filter(x -> x.getId() == id).findFirst();
+    }
+
+    public List<Post> searchPosts(String author) {
+        return posts.stream().filter(
+                post -> Objects.equals(post.getAuthor(), author)
+        ).collect(Collectors.toList());
+    }
+
+    public List<Post> searchPosts(String author, LocalDate date) {
+        return posts.stream().filter(
+                        post -> Objects.equals(post.getAuthor(), author)
+                ).filter(post -> post.getCreationDate().atZone(ZoneId.systemDefault()).toLocalDate().equals(date))
+                .collect(Collectors.toList());
+    }
+
+    public List<Post> listPosts(Integer userId, LocalDate from, LocalDate to) {
+        return posts.stream()
+                .filter(post -> post.getId() == userId)
+                .filter(post -> post.getCreationDate().atZone(ZoneId.systemDefault()).toLocalDate().isAfter(from))
+                .filter(post -> post.getCreationDate().atZone(ZoneId.systemDefault()).toLocalDate().isBefore(to))
+                .collect(Collectors.toList());
     }
 }
